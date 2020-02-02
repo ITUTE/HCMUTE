06ac63e65b52f9b9963b58542fb2fdf50a8764c5
Nguyễn Huỳnh Minh Tiến <33385777+Titytus@users.noreply.github.com>
2020-02-02 20:29:38 +0700

    add

---
+++ Nhập môn lập trình - Introduction to Programming/Chapter 4/Python 3/Untitled-1.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+#import numpy as np
+
+def f(x):
+    return x**2 + 2*x + 1
+
+#x_1=np.linspace(start=-3,stop=3,num=100)
+#y_1=f(x_1)
+#plt.plot(x_1,y_1)
+#plt.show()
